0e0b694647ba54b77a84fb385fcd08030b832f60
ligerlac <lino.oscar.gerlach@cern.ch>
2022-11-04 10:20:22 -0400

    added cli get executable, made it configurable

--- executables/client_lino.sh
+++ executables/client_lino.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
-for i in $(eval echo {1..$1})
+useage="$0 <nopayloadclient conf name> <n_calls>"
+
+export NOPAYLOADCLIENT_CONF=$1
+
+for i in $(eval echo {1..$2})
 do
     start=`date +%s.%N`
-    res=./test_get
+    ./cli_get sPHENIX_ExampleGT_1 Beam 0 0
     end=`date +%s.%N`
     runtime=$( echo "$end - $start" | bc -l)
     echo runtime=$runtime
-    echo httpcode=200
 done
--- executables/curl_lino.sh
+++ executables/curl_lino.sh
@@ -3,7 +3,7 @@
 for i in $(eval echo {1..$1})
 do
     start=`date +%s.%N`
-    httpcode=$(curl --write-out '%{http_code}' --silent --output /dev/null http://linostest.apps.usatlas.bnl.gov/api/cdb_rest/gt)
+    httpcode=$(curl --write-out '%{http_code}' --silent --output /dev/null http://test111.apps.usatlas.bnl.gov/api/cdb_rest/gt)
     end=`date +%s.%N`
     runtime=$( echo "$end - $start" | bc -l)
     echo runtime=$runtime
